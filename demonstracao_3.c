b8117274ad47e2f1842ffad9f89433641aa305f2
Rafael Araújo <rafael21_psp@hotmail.com>
2019-11-24 18:38:09 -0300

    #4

--- demonstracao_3.c
+++ demonstracao_3.c
@@ -16,61 +16,129 @@ typedef struct TipoGrafo {
   int NumArestas;
 } TipoGrafo;
 
-typedef struct TipoItem {
-    TipoVertice vertice;
-} TipoItem;
-
-typedef struct TipoNo {
-    TipoItem item;
-    TipoNo *prox;
-} TipoNo;
-
-typedef struct TipoLista {
-    TipoNo *primeiro, *ultimo;
-} TipoLista;
+int INFINITO = __INT_MAX__;
 
 typedef TipoGrafo* TipoApontador;
 
-void InicializarLista(TipoLista *Lista)
+void ImprimirMatriz(int NumVertices, TipoPeso Matriz[NumVertices][NumVertices])
 {
-    Lista->primeiro = (TipoNo*) malloc(sizeof(TipoNo)); 
-    Lista->ultimo = Lista->primeiro;
-    Lista->primeiro->prox = NULL;
+    for (int i = 0; i < NumVertices; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < NumVertices; j++)
+        {
+            printf("%3ld\t", Matriz[i][j]);
+        }
+    }
 }
 
-int ListaVazia(TipoLista* Lista)
+void ImprimirCaminho(int NumVertices, int Caminho[NumVertices])
 {
-    return (Lista->primeiro == Lista->ultimo);
+    for (int i = 0; i < NumVertices; i++)
+    {
+        if(i < NumVertices-1)
+        {
+            printf("%d -> ", Caminho[i]);
+        }
+        else
+        {
+            printf("%d -> %d", Caminho[i], Caminho[i-(NumVertices-1)]);
+        }
+        
+    }
 }
 
-void InserirItem(TipoItem item, TipoLista* Lista)
+void CopiarMatrizGrafo(TipoGrafo* Grafo, int NumVertices, TipoPeso Matriz[NumVertices][NumVertices])
 {
-    Lista->ultimo->prox = (TipoNo*) malloc(sizeof(TipoNo));
-    Lista->ultimo = Lista->ultimo->prox;
-    Lista->ultimo->item = item;
-    Lista->ultimo->prox = NULL;
+    for (int i = 0; i < NumVertices; i++)
+    {
+        
+        for (int j = 0; j < NumVertices; j++)
+        {
+            if(Grafo->Mat[i][j] != 0)
+            {
+                Matriz[i][j] = Grafo->Mat[i][j];
+            }
+            else if(i == j)
+            {
+                Matriz[i][j] = 0;
+            }
+            else
+            {
+                Matriz[i][j] = 4294967295;
+            }
+            
+        }
+    }
 }
 
-void RemoverItem(TipoNo* item, TipoLista *Lista)
-{ /*  ---   Obs.: o item a ser retirado e  o seguinte ao apontado por  p --- */
-  TipoNo *aux;
-  
-  if (Vazia(*Lista) || item == NULL || item->prox == NULL) 
-  { 
-    printf(" Erro: lista vazia ou indice inexistente\n");
-    return;
-  }
-  
-  aux = item->prox;
-  item->prox = aux->prox;
-  
-  if (item->prox == NULL)
-  {
-    Lista->ultimo = item;
-  } 
+TipoPeso CalculaCustoCaminho(int NumVertices, int Caminho[NumVertices], TipoPeso Matriz[NumVertices][NumVertices])
+{
+    TipoPeso CustoCaminho = 0;
 
+    for(int i = 0; i<NumVertices; i++){
+        if(i < NumVertices-1)
+        {
+            CustoCaminho += Matriz[Caminho[i]][Caminho[i+1]];
+        }
+        else
+        {
+            CustoCaminho += Matriz[Caminho[i]][Caminho[i-(NumVertices-1)]];
+        }
+    }
 
-  free(aux);
+    return CustoCaminho;
+}
+
+void Dijkstra(TipoGrafo *Grafo)
+{
+    int NumVertices = Grafo->NumVertices;
+    TipoPeso Matriz[NumVertices][NumVertices];
+
+    CopiarMatrizGrafo(Grafo, NumVertices, Matriz);
+
+    int Visitados[NumVertices];
+    int Caminho[NumVertices];
+    TipoPeso CustoCaminho = 0;
+
+    for (int i = 0; i < NumVertices; i++)
+    {
+        Visitados[i] = -1;
+        Caminho[i] = 0;
+    }
+
+    for (int i = 0; i < NumVertices; i++)
+    {
+        int min = -1;
+        TipoPeso MinValor = INFINITO;
+
+        for (int j = 0; j < NumVertices; j++)
+        {
+            if( Visitados[j] != 1 && Matriz[0][j] < MinValor)
+            {
+                min = j;
+                MinValor = Matriz[0][j];
+            }
+        }
+        
+        Visitados[min] = 1;
+        Caminho[i] = min;
+
+        for (int j = 0; j < NumVertices; j++)
+        {
+            if((Matriz[0][min] + Matriz[j][min]) < Matriz[0][j])
+            {
+                Matriz[0][j] = Matriz[0][min] + Matriz[j][min];
+            }
+        }
+        
+    }
+
+    printf("Caminho a ser percorrido: ");
+    ImprimirCaminho(NumVertices, Caminho);
+    putchar('\n');
+    CustoCaminho = CalculaCustoCaminho(NumVertices, Caminho, Matriz);
+    printf("\nCusto total do caminho: %3ld", CustoCaminho);
 }
 
 void GerarGrafoVazio(TipoGrafo *Grafo)
@@ -149,37 +217,51 @@ void BuscaProfundidade(TipoGrafo *Grafo)
 
 void CarregarGrafo(TipoGrafo *Grafo)
 {
-    TipoVertice V0=0, V1=1, V2=2, V3=3, V4=4, V5=5, V6=6;
-    TipoPeso Peso1 = 1, Peso2 = 2, Peso4 = 4, Peso6 = 6;
+    TipoVertice V0=0, V1=1, V2=2, V3=3, V4=4, V5=5;
+    TipoPeso Peso3 = 3, Peso4 = 4, Peso5 = 5, Peso7 = 7, Peso8 = 8, Peso9 = 9,
+            Peso10 = 10, Peso11 = 11, Peso12 = 12, Peso15 = 15, Peso18 = 18,
+            Peso20 = 20, Peso25 = 25, Peso26 = 26;
 
-    InsereAresta(&V0, &V5, &Peso2, Grafo);
-    InsereAresta(&V0, &V1, &Peso1, Grafo);
-    InsereAresta(&V0, &V6, &Peso6, Grafo);
-    InsereAresta(&V5, &V0, &Peso2, Grafo);
-    InsereAresta(&V5, &V3, &Peso1, Grafo);
-    InsereAresta(&V5, &V4, &Peso2, Grafo);
-    InsereAresta(&V3, &V5, &Peso1, Grafo);
-    InsereAresta(&V3, &V4, &Peso2, Grafo);
-    InsereAresta(&V3, &V1, &Peso2, Grafo);
-    InsereAresta(&V1, &V0, &Peso1, Grafo);
-    InsereAresta(&V1, &V3, &Peso2, Grafo);
-    InsereAresta(&V1, &V2, &Peso1, Grafo);
-    InsereAresta(&V1, &V4, &Peso4, Grafo);
-    InsereAresta(&V2, &V1, &Peso1, Grafo);
+    InsereAresta(&V0, &V1, &Peso3, Grafo);
+    InsereAresta(&V0, &V2, &Peso10, Grafo);
+    InsereAresta(&V0, &V3, &Peso11, Grafo);
+    InsereAresta(&V0, &V4, &Peso7, Grafo);
+    InsereAresta(&V0, &V5, &Peso25, Grafo);
+
+    InsereAresta(&V1, &V0, &Peso3, Grafo);
+    InsereAresta(&V1, &V2, &Peso8, Grafo);
+    InsereAresta(&V1, &V3, &Peso12, Grafo);
+    InsereAresta(&V1, &V4, &Peso9, Grafo);
+    InsereAresta(&V1, &V5, &Peso26, Grafo);
+
+    InsereAresta(&V2, &V0, &Peso10, Grafo);
+    InsereAresta(&V2, &V1, &Peso8, Grafo);
+    InsereAresta(&V2, &V3, &Peso9, Grafo);
     InsereAresta(&V2, &V4, &Peso4, Grafo);
-    InsereAresta(&V6, &V0, &Peso6, Grafo);
-    InsereAresta(&V6, &V4, &Peso4, Grafo);
-    InsereAresta(&V4, &V5, &Peso2, Grafo);
-    InsereAresta(&V4, &V3, &Peso2, Grafo);
-    InsereAresta(&V4, &V1, &Peso4, Grafo);
-    InsereAresta(&V4, &V2, &Peso4, Grafo);
-    InsereAresta(&V4, &V6, &Peso1, Grafo);
+    InsereAresta(&V2, &V5, &Peso20, Grafo);
 
+    InsereAresta(&V3, &V0, &Peso11, Grafo);
+    InsereAresta(&V3, &V1, &Peso12, Grafo);
+    InsereAresta(&V3, &V2, &Peso9, Grafo);
+    InsereAresta(&V3, &V4, &Peso5, Grafo);
+    InsereAresta(&V3, &V5, &Peso15, Grafo);
+
+    InsereAresta(&V4, &V0, &Peso7, Grafo);
+    InsereAresta(&V4, &V1, &Peso9, Grafo);
+    InsereAresta(&V4, &V2, &Peso4, Grafo);
+    InsereAresta(&V4, &V3, &Peso5, Grafo);
+    InsereAresta(&V4, &V5, &Peso18, Grafo);
+
+    InsereAresta(&V5, &V0, &Peso25, Grafo);
+    InsereAresta(&V5, &V1, &Peso26, Grafo);
+    InsereAresta(&V5, &V2, &Peso20, Grafo);
+    InsereAresta(&V5, &V3, &Peso15, Grafo);
+    InsereAresta(&V5, &V4, &Peso18, Grafo);
 }
 
 int main(){
     TipoGrafo Grafo;
-    Grafo.NumVertices = 7;
+    Grafo.NumVertices = 6;
 
     TipoApontador Apontador = &Grafo;
 
@@ -188,6 +270,6 @@ int main(){
     ImprimeGrafo(Apontador);
     printf("======================\n");
     printf("~~~~~~Resultados~~~~~~\n");
-    BuscaProfundidade(Apontador);
+    Dijkstra(Apontador);
     printf("\n~~~~~~~~~~~~~~~~~~~~~~\n");
 }
